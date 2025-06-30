ada3b5fb25a11074730376948c184888712a3be0
cmclaughlin24 <curtismclaughlin91@gmail.com>
2025-06-30 16:15:27 -0500

    add symlinks for .tmux.conf and .zshrc

--- init.sh
+++ init.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
-ln -s $PWD/config/aerospace ~/.config/aerospace
-ln -s $PWD/config/ghostty ~/.config/ghostty
-ln -s $PWD/config/nvim ~/.config/nvim
+# Symlink Configuration 
+if [[ ! -L ~/.config/aerospace ]]; then
+  ln -s "$PWD/config/aerospace" ~/.config/aerospace
+fi
+
+if [[ ! -L ~/.config/ghostty ]]; then
+  ln -s "$PWD/config/ghostty" ~/.config/ghostty
+fi
+
+if [[ ! -L ~/.config/nvim ]]; then
+  ln -s "$PWD/config/nvim" ~/.config/nvim
+fi
+
+if [[ ! -L ~/.zshrc ]]; then
+  ln -s "$PWD/config/.zshrc" ~/.zshrc
+fi
+
+if [[ ! -L ~/.tmux.conf ]]; then
+  ln -s "$PWD/config/.tmux.conf" ~/.tmux.conf
+fi
