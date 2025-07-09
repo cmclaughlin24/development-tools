20cf40143d0edec36ba3707f5cf3fe9cd367eaf6
cmclaughlin24 <curtismclaughlin91@gmail.com>
2025-07-08 19:26:19 -0500

    update init.sh

--- init.sh
+++ init.sh
@@ -4,19 +4,33 @@
 if ! command -v brew &>/dev/null; then
     echo "Homebrew is not installed, installing now..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed"
 fi
+
+# Add Additional Repositories (Taps)
+# TAPS=("isen-ng/dotnet-sdk-versions")
+#
+# for TAP in "${TAPS[@]}"; do
+#     if ! brew tap | grep -q "^$TAP\$"; then
+#         echo "$TAP is not tapped, tapping now..."
+#         brew tap $TAP
+#     else
+#         echo "$TAP is already tapped"
+#     fi
+# done
 
 # Install Homebrew Formulae
 FORMULAE=(
-    "fzf" 
-    "go" 
-    "helm" 
-    "jq" 
-    "kubernetes-cli" 
-    "lazygit" 
-    "minikube" 
-    "neovim" 
-    "node" 
+    "fzf"
+    "go"
+    "helm"
+    "jq"
+    "kubernetes-cli"
+    "lazygit"
+    "minikube"
+    "neovim"
+    "node"
     "powerlevel10k"
     "ripgrep"
     "tmux"
@@ -26,46 +40,73 @@ FORMULAE=(
 )
 
 for FORMULA in "${FORMULAE[@]}"; do
-    if ! brew list $FORMULA &>/dev/null; then 
+    if ! brew list $FORMULA &>/dev/null; then
         echo "$FORMULA is not installed, installing now..."
         brew install $FORMULA
     else
-        echo "$FORMULA is already installed."
+        echo "$FORMULA is already installed"
     fi
 done
 
-# Install Homebrew Cask
+# Install Homebrew Casks
 CASKS=(
     "aerospace"
+    "docker"
+    # "dotnet-sdk8"
+    # "dotnet-sdk8-0-400"
+    "finch"
     "ghostty"
+    "insomnia"
+    "redis-insight"
+    "visual-studio-code"
 )
 
 for CASK in "${CASKS[@]}"; do
-    if ! brew list --cask $CASK &>/dev/null; then 
+    if ! brew list --cask $CASK &>/dev/null; then
         echo "$CASK is not installed, installing now..."
         brew install --cask $CASK
     else
-        echo "$CASK is already installed."
+        echo "$CASK is already installed"
     fi
 done
 
-# Symlink Configuration 
-if [[ ! -L ~/.config/aerospace ]]; then
-    ln -s "$PWD/config/aerospace" ~/.config/aerospace
+# Install Oh My Zsh
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    echo "Oh My Zsh is not installed, installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh is already installed"
 fi
 
-if [[ ! -L ~/.config/ghostty ]]; then
-    ln -s "$PWD/config/ghostty" ~/.config/ghostty
-fi
+# Symlink Configuration
+SYMLINKS=(
+    "$PWD/config/aerospace:~/.config/aerospace"
+    "$PWD/config/ghostty:~/.config/ghostty"
+    "$PWD/config/nvim:~/.config/nvim"
+    "$PWD/config/.zshrc:~/.zshrc"
+    "$PWD/config/.p10k.zsh:~/.p10k.zsh"
+    "$PWD/config/.tmux.conf:~/.tmux.conf"
+)
 
-if [[ ! -L ~/.config/nvim ]]; then
-    ln -s "$PWD/config/nvim" ~/.config/nvim
-fi
+for SYMLINK in "${SYMLINKS[@]}"; do
+    SOURCE=$(echo "$SYMLINK" | cut -d':' -f1)
+    TARGET=$(echo "$SYMLINK" | cut -d':' -f2)
 
-if [[ ! -L ~/.zshrc ]]; then
-    ln -s "$PWD/config/.zshrc" ~/.zshrc
-fi
+    # NOTE: Need to expand ~ to the user's home directory (cases where ~ may not be
+    # interpreted correctly).
+    TARGET="${TARGET/#~/$HOME}"
 
-if [[ ! -L ~/.tmux.conf ]]; then
-    ln -s "$PWD/config/.tmux.conf" ~/.tmux.conf
-fi
+    if [[ ! -L "$TARGET" ]]; then
+        echo "Creating symlink: $TARGET -> $SOURCE"
+        ln -s "$SOURCE" "$TARGET"
+
+        # NOTE: Special cases for sourcing configuration files.
+        if [[ "$TARGET" == "$HOME/.zshrc" ]]; then
+            source "$TARGET"
+        elif [[ "$TARGET" == "$HOME/.tmux.conf" ]]; then
+            tmux source "$TARGET"
+        fi
+    else
+        echo "Symlink already exists: $TARGET"
+    fi
+done
